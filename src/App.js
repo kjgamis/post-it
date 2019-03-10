da5cbfef539a88c2c68134dcc1232c9c19b2029c
kjgamis <kjgamis@live.ca>
2019-03-09 20:09:05 -0500

    add jquery and materialize

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import {
   Switch
 } from 'react-router-dom'
 
+import Navigation from './components/layout/Navigation'
 import Dashboard from './components/dashboard/Dashboard'
 import Create from './components/posts/CreatePost'
 import SignIn from './components/auth/SignIn'
@@ -14,12 +15,13 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <Navigation/>
+        <Dashboard/>
         <Switch>
           <Route path='/create' component={Create} />
           <Route path='/signin' component={SignIn} />
           <Route path='/signup' component={SignUp} />
         </Switch>
-        <Dashboard/>
       </div>
     );
   }
--- src/components/layout/Navigation.js
+++ src/components/layout/Navigation.js
@@ -1,8 +1,18 @@
 import React from 'react'
 
+import {Link} from 'react-router-dom'
+
+import SignedInLinks from './SignedInLinks'
+import SignedOutLinks from './SignedOutLinks'
+
 const Navigation = () => (
   <div className="navigation">
-    <p>This is the Navigation Bar</p>
+    <Link to='/create'>Create Post</Link><br/>
+    <Link to='/signin'>Sign In</Link><br/>
+    <Link to='/signup'>Sign Up</Link><br/>
+
+    <SignedInLinks/>
+    <SignedOutLinks/>
   </div>
 )
 
