fb73c574ca341d0573eabb28b7a7d532b9bf169c
kjgamis <kjgamis@live.ca>
2019-03-21 23:33:29 -0400

    add redux thunk middleware, postAction - createPost, submit handle to add new post with async dipatch

--- src/store/reducers/postReducer.js
+++ src/store/reducers/postReducer.js
@@ -7,6 +7,9 @@ const initState = {
 }
 
 const postReducer = (state = initState, action) => {
+    switch (action.type) {
+        case 'CREATE_POST': console.log(`created post`, action.post)
+    }
     return state
 }
 
