8a16770cb828f803f990d4636f1b79cd5850fba7
kjgamis <kjgamis@live.ca>
2019-03-24 14:56:18 -0400

    finish thunk setup for now

--- src/store/actions/postActions.js
+++ src/store/actions/postActions.js
@@ -1,4 +1,5 @@
 export const createPost = (post) => {
+    // dispatch is the function that dispatches an action to the reducer 
     return (dispatch, getState) => {
         // make asyc call to database 
         dispatch({
