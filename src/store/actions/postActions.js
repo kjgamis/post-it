624a9cda8c404f35405393c5091589e92a113071
kjgamis <kjgamis@live.ca>
2019-04-02 22:29:10 -0400

    Add store enhancers from react-redux-firebase and redux-firestore

--- src/store/actions/postActions.js
+++ src/store/actions/postActions.js
@@ -1,6 +1,6 @@
 export const createPost = (post) => {
     // dispatch is the function that dispatches an action to the reducer 
-    return (dispatch, getState) => {
+    return (dispatch, getState, { getFirebase, getFirestore }) => {
         // make asyc call to database 
         dispatch({
             type: 'CREATE_POST',
