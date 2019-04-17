d1ca69a4f704e2bad3084a3305f55170d10d4667
kjgamis <kjgamis@live.ca>
2019-04-16 21:31:11 -0400

    add auth ready to remove flicker when rendering page with signedin/signedout logic

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ import rootReducer from './store/reducers/rootReducer'
 import { reduxFirestore, getFirestore } from 'redux-firestore'
 import { reactReduxFirebase, getFirebase } from 'react-redux-firebase'
 import firebaseConfig from './config/firebaseConfig'
+import rrfConfig from './config/rrfConfig'
 
 // extra arguments so that in postActions we can add the getFirebase and getFirestore in the return argument
 const store = createStore(
@@ -20,11 +21,15 @@ const store = createStore(
             thunk.withExtraArgument({getFirebase, getFirestore})
         ),
         reduxFirestore(firebaseConfig),
-        reactReduxFirebase(firebaseConfig)
+        reactReduxFirebase(firebaseConfig, rrfConfig)
     )
 )
 
-ReactDOM.render((<Provider store={store}><App /></Provider>), document.getElementById('root'));
+// Listen for auth ready (promise available on store thanks to attachAuthIsReady: true config option)
+store.firebaseAuthIsReady.then(() => {
+    console.log('Auth has loaded') // eslint-disable-line no-console
+    ReactDOM.render((<Provider store={store}><App /></Provider>), document.getElementById('root'));
+})
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
