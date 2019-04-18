d2a9f2940253275dd228e7fabc839876eb20fc85
kjgamis <kjgamis@live.ca>
2019-04-18 00:25:23 -0400

    add post error message to state

--- src/store/reducers/postReducer.js
+++ src/store/reducers/postReducer.js
@@ -3,17 +3,21 @@ const initState = {
     //     {id: 1, title: 'First Post', content: '', user: 'KJG'},
     //     {id: 2, title: 'Refactor', content: '', user: 'Kage'},
     //     {id: 3, title: 'Eat Ice Cream', content: '', user: 'Stranger'}
-    ]
+    ],
+    postError: null
 }
 
 const postReducer = (state = initState, action) => {
     switch (action.type) {
         case 'CREATE_POST': 
             console.log(`created post`, action.post)
-            return state;            
+            return state
         case 'CREATE_POST_ERROR':
             console.log(`Create Post Error`, action.err)
-            return state
+            return {
+                ...state,
+                postError: action.err.message
+            }
         default:
             return state
     }
